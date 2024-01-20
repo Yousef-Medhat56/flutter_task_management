1a23bcbcb97d5109473e221f9eda01e830f5b003
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2024-01-20 09:55:33 +0200

    feat: add screen 2

--- lib/features/onboarding/widgets/screens/screen_two.dart
+++ lib/features/onboarding/widgets/screens/screen_two.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:task_management/common/utils/constants.dart';
+import 'package:task_management/common/widgets/buttons/outline_button.dart';
+import 'package:task_management/common/widgets/spacers/height_spacer.dart';
+import 'package:task_management/features/auth/pages/login_page.dart';
 
 class ScreenTwo extends StatelessWidget {
   const ScreenTwo({super.key});
@@ -9,7 +13,29 @@ class ScreenTwo extends StatelessWidget {
     return Container(
       width: AppConsts.kWidth,
       height: AppConsts.kHeight,
-      color: AppConsts.kBlueLight,
+      color: AppConsts.kBkDark,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 40.w,
+            ),
+            child: Image.asset("assets/images/todo.png"),
+          ),
+          const HeightSpacer(height: 60),
+          OutlineBtn(
+              onTap: () {
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) => const LoginPage()));
+              },
+              width: AppConsts.kWidth * 0.9,
+              height: AppConsts.kHeight * 0.06,
+              borderColor: AppConsts.kLight,
+              text: "Login with phone number")
+        ],
+      ),
     );
   }
 }
