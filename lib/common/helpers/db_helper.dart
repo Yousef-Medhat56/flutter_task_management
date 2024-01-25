7bf76c931c7f20a0ca0e2bf0f72778134577189e
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2024-01-25 10:37:56 +0200

    feat(models): add crud methods

--- lib/common/helpers/db_helper.dart
+++ lib/common/helpers/db_helper.dart
@@ -1,6 +1,9 @@
+import "package:flutter/material.dart";
 import "package:sqflite/sqflite.dart";
+import "package:task_management/common/models/task_model.dart";
 
 class DBHelper {
+  // create the tables
   static Future<void> createTables(Database db) async {
     //create Todo table
     await db.execute(
@@ -26,6 +29,7 @@ class DBHelper {
     );
   }
 
+  // return the database
   static Future<Database> db() async {
     return openDatabase(
       "task_managment",
@@ -34,5 +38,67 @@ class DBHelper {
         await createTables(database);
       },
     );
+  }
+
+  //insert new todo item
+  static Future<int> insertTodo(TaskModel task) async {
+    final Database db = await DBHelper.db();
+    final int id = await db.insert("Todo", task.toJson(),
+        conflictAlgorithm: ConflictAlgorithm.replace);
+    return id;
+  }
+
+  //insert new user
+  static Future<int> insertUser(int isVerified) async {
+    final Database db = await DBHelper.db();
+
+    final data = {"id": 0, "isVerified": isVerified};
+    final int id = await db.insert("User", data,
+        conflictAlgorithm: ConflictAlgorithm.replace);
+    return id;
+  }
+
+  // get users
+  static Future<List<Map<String, dynamic>>> getUsers() async {
+    final Database db = await DBHelper.db();
+    return db.query("User", orderBy: "id");
+  }
+
+  // get todos
+  static Future<List<Map<String, dynamic>>> getTodos() async {
+    final Database db = await DBHelper.db();
+    return db.query("Todo", orderBy: "id");
+  }
+
+  // get a todo
+  static Future<List<Map<String, dynamic>>> getTodo(int id) async {
+    final Database db = await DBHelper.db();
+    return db.query("Todo", where: "id=?", whereArgs: [id], limit: 1);
+  }
+
+  //update a todo
+  static Future<int> updateTodo(int id, TaskModel updatedTask) async {
+    final Database db = await DBHelper.db();
+    final results = db.update(
+      "Todo",
+      updatedTask.toJson(),
+      where: "id=?",
+      whereArgs: [id],
+    );
+    return results;
+  }
+
+  //delete a todo
+  static Future<void> deleteTodo(int id) async {
+    final Database db = await DBHelper.db();
+    try {
+      await db.delete(
+        "Todo",
+        where: "id=?",
+        whereArgs: [id],
+      );
+    } catch (err) {
+      debugPrint("Error: $err");
+    }
   }
 }
