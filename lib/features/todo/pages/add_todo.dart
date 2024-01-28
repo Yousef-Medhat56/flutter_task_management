6afcee6021e8d23f85f0bb78f826cf809736ed97
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2024-01-28 13:22:01 +0200

    feat(add todo): add onConfirm handler to date and time pickers

--- lib/features/todo/pages/add_todo.dart
+++ lib/features/todo/pages/add_todo.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:task_management/common/models/task_model.dart';
 import 'package:task_management/common/utils/constants.dart';
 import 'package:task_management/common/widgets/appstyle.dart';
 import 'package:task_management/common/widgets/buttons/outline_button.dart';
@@ -9,6 +10,7 @@ import 'package:task_management/common/widgets/spacers/height_spacer.dart';
 import 'package:flutter_datetime_picker_plus/flutter_datetime_picker_plus.dart'
     as picker;
 import 'package:task_management/features/todo/controllers/dates/dates_provider.dart';
+import 'package:task_management/features/todo/controllers/todo/todo_provider.dart';
 
 class AddTodoPage extends ConsumerStatefulWidget {
   const AddTodoPage({super.key});
@@ -76,11 +78,11 @@ class _AddTodoPageState extends ConsumerState<AddTodoPage> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 OutlineBtn(
-                  //TODO: Add tap handler
                   onTap: () {
-                    picker.DatePicker.showTime12hPicker(
+                    picker.DatePicker.showTimePicker(
                       context,
                       showTitleActions: true,
+                      showSecondsColumn: false,
                       onConfirm: (date) {
                         ref.read(startTimeStateProvider.notifier).setStartTime(
                               date.toString().substring(10, 16),
@@ -89,7 +91,6 @@ class _AddTodoPageState extends ConsumerState<AddTodoPage> {
                       currentTime: DateTime.now(),
                     );
                   },
-
                   width: AppConsts.kWidth * 0.45,
                   height: 52.h,
                   borderColor: AppConsts.kLight,
@@ -97,11 +98,11 @@ class _AddTodoPageState extends ConsumerState<AddTodoPage> {
                   text: todoStartTime.isNotEmpty ? todoStartTime : "Start time",
                 ),
                 OutlineBtn(
-                  //TODO: Add tap handler
                   onTap: () {
-                    picker.DatePicker.showTime12hPicker(
+                    picker.DatePicker.showDateTimePicker(
                       context,
                       showTitleActions: true,
+                      minTime: DateTime.now(),
                       onConfirm: (date) {
                         ref
                             .read(finishTimeStateProvider.notifier)
@@ -122,8 +123,33 @@ class _AddTodoPageState extends ConsumerState<AddTodoPage> {
             ),
             const HeightSpacer(height: 20),
             OutlineBtn(
-              //TODO: Add tap handler
-              onTap: () {},
+              onTap: () {
+                if (todoTitleController.text.isNotEmpty &&
+                    todoDescController.text.isNotEmpty &&
+                    todoDate.isNotEmpty &&
+                    todoStartTime.isNotEmpty &&
+                    todoFinishTime.isNotEmpty) {
+                  TaskModel task = TaskModel(
+                      title: todoTitleController.text,
+                      desc: todoDescController.text,
+                      date: todoDate,
+                      startTime: todoStartTime,
+                      endTime: todoFinishTime,
+                      isCompleted: 0,
+                      remind: 0,
+                      repeat: "yes");
+                  //add new task
+                  ref.read(todoStateProvider.notifier).addTodo(task);
+                  //clear date and time providers
+                  ref.read(dateStateProvider.notifier).setDate("");
+                  ref.read(startTimeStateProvider.notifier).setStartTime("");
+                  ref.read(finishTimeStateProvider.notifier).setFinishTime("");
+
+                  Navigator.pop(context);
+                } else {
+                  debugPrint("Failed to insert new task");
+                }
+              },
               width: AppConsts.kWidth,
               height: 52.h,
               borderColor: AppConsts.kLight,
